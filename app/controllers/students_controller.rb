f0fe1c84cac09a5ee6e4ca246c1cd5512bbb43cb
Michael Iodice <miodice3@gmail.com>
2020-11-27 12:21:52 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,13 @@ class StudentsController < ApplicationController
   end
 
   def create
+#    <Student id: nil, first_name: nil, last_name: nil, created_at: nil, updated_at: nil> 
+    #byebug
+    student = Student.new
+    student.first_name = params[:first_name]
+    student.last_name = params[:last_name]
+    student.save
+    redirect_to student
   end
 
 end
